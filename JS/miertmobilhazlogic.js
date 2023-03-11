053782174a419a42494c211d6de4a793f2b65d1a
webtonerweblap <126803784+webtonerweblap@users.noreply.github.com>
2023-03-12 00:06:16 +0100

    Add files via upload

--- JS/miertmobilhazlogic.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const imageHeight = document.getElementById("kezdokep").offsetHeight;
-    document.getElementById("segment1-textholder").style.height = imageHeight + "px";
-  });
